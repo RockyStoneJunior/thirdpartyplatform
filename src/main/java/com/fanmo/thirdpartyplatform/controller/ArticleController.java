f6fd61b5355b2c8b6902f22f3f7f44fa12e81506
Administrator <511697856@qq.com>
2019-08-09 19:12:10 +0800

    添加了将AccessToken写入数据库的接口，并且添加了定时任务。定时任务需要在启动类上面添加@EnableScheduling 注解
    Signed-off-by: Administrator <511697856@qq.com>

---
+++ src/main/java/com/fanmo/thirdpartyplatform/controller/ArticleController.java
@@ -0,0 +1,10 @@
+package com.fanmo.thirdpartyplatform.controller;
+
+import com.fanmo.thirdpartyplatform.persistence.dao.AccessTokenRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class ArticleController {
+    @Autowired
+    AccessTokenRepository accessTokenRepository;
+
+}
